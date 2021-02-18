4943a431dc8d48e7189d86568252adb9fa97b9bd
alfreddevelops <narutoalf@yahoo.com>
2021-02-18 22:04:12 +0800

    changed explore page
    added individual page for each food

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 $(function () {
   $("#hamburger-menu").click(openMenu);
+
+  // EXPLORE PAGE OVERLAY
+  $("#closeOverlay").click(closeOverlay);
+  $(".explore-brotherbird").click(showFood1);
+
 })
 
 // var navbar = document.createElement('style');
@@ -16,34 +21,76 @@ function openMenu() {
   $('#navigations').toggleClass("open");
 }
 
+// $('.explore-carousel').slick({
+//   infinite: true,
+//   slidesToShow: 4,
+//   slidesToScroll: 4
+// });
+
 $('.explore-carousel').slick({
-  infinite: true,
+  centerMode: true,
+  centerPadding: '50px',
   slidesToShow: 3,
-  slidesToScroll: 3
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
 });
 
-// $('.explore-carousel').slick({
-//   centerMode: true,
-//   centerPadding: '10px',
-//   slidesToShow: 3,
-//   responsive: [
-//     {
-//       breakpoint: 768,
-//       settings: {
-//         arrows: false,
-//         centerMode: true,
-//         centerPadding: '40px',
-//         slidesToShow: 3
-//       }
-//     },
-//     {
-//       breakpoint: 480,
-//       settings: {
-//         arrows: false,
-//         centerMode: true,
-//         centerPadding: '40px',
-//         slidesToShow: 1
-//       }
-//     }
-//   ]
-// });
+function closeOverlay() {
+  $(".food-overlay").removeClass("appear");
+  $(".explore-carousel").css("opacity", "1");
+}
+
+function showFood1() {
+  // info of location
+  var brotherbird = {
+    foodImage: "img/brotherbird/mochi-donut.jpg",
+    foodTitle: "Brotherbird Coffeehouse",
+    foodType: "Shophouse Cafe",
+    foodLocation: "32 Bali Ln, Singapore 189868",
+    foodDescription: "Brotherbird Coffeehouse serves a huge variety of croissants. And not just that, they also serve pasta and other desserts.",
+
+    thumbnail1: "img/brotherbird/drinks.jpg",
+    thumbnail2: "img/brotherbird/spaghetti-carbonara.jpg",
+    thumbnail3: "img/brotherbird/croissant.jpg",
+    // thumbnail3: "images/works/cross-roads/instructions.jpg",
+    // thumbnail4: "images/works/cross-roads/hand.jpg",
+  };
+
+  $(".explore-carousel").css("opacity", "0.5");
+  $(".food-overlay").addClass("appear");
+
+  $(".food-overlay .main-image").css("background-image", "url(" + brotherbird.foodImage + ")");
+  $(".overlay-title").html(brotherbird.foodTitle);
+  // $(".overlay-type").html(brotherbird.foodType);
+
+  var pin = document.createElement("img");
+  pin.src = "img/icons/pin.png";
+  var src = document.getElementById("food-location");
+  src.appendChild(pin);
+  $(".overlay-location").append(brotherbird.foodLocation);
+  // $(".overlay-location").html(brotherbird.foodLocation);
+
+  $(".overlay-description").html(brotherbird.foodDescription);
+
+  $("#thumbnail-1").attr("src", brotherbird.thumbnail1);
+  $("#thumbnail-2").attr("src", brotherbird.thumbnail2);
+  $("#thumbnail-3").attr("src", brotherbird.thumbnail3);
+}
