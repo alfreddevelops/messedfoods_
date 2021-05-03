4a40a45bf4be597ff66b1dceb41b96eee5a9d291
alfred <narutoalf@yahoo.com>
2021-05-03 19:22:43 +0800

    changes

--- public/js/index.js
+++ public/js/index.js
@@ -74,7 +74,9 @@ function foodData() {
       //TYPE ALL THE CODES IN HERE
       food = json;
 
+      $(".explore-images").click(cssChanges);
       $(".explore-brotherbird").click(brotherbirdInfo);
+      $(".explore-haakon").click(haakonInfo);
 
     })
     .fail(function () {
@@ -83,14 +85,16 @@ function foodData() {
 
 }
 
-// Brotherbird Coffeehouse
-function brotherbirdInfo() {
-  // $("footer").css("display", "none");
+// Repeated css changes
+function cssChanges() {
   $(".explore-carousel").css("opacity", "0.5");
   $(".food-info").css("display", "flex");
   $("#hamburger-menu").css("display", "none");
   $(".food-overlay").addClass("appear");
+}
 
+// Brotherbird Coffeehouse
+function brotherbirdInfo() {
   $(".food-overlay .main-image").css("background-image", "url(" + food[0].brotherbird.foodImage + ")");
   $(".overlay-title").html(food[0].brotherbird.shopName);
   $(".overlay-rating").append(food[0].brotherbird.foodRating);
@@ -101,7 +105,6 @@ function brotherbirdInfo() {
   var src = document.getElementById("food-location");
   src.appendChild(pin);
   $(".overlay-location").append(food[0].brotherbird.shopLocation);
-  // $(".overlay-location").html(brotherbird.foodLocation);
 
   $(".overlay-description").html(food[0].brotherbird.foodDescription);
 
@@ -109,3 +112,22 @@ function brotherbirdInfo() {
   $("#thumbnail-2").attr("src", food[0].brotherbird.thumbnail2);
   $("#thumbnail-3").attr("src", food[0].brotherbird.thumbnail3);
 }
+
+ function haakonInfo() {
+  $(".food-overlay .main-image").css("background-image", "url(" + food[2].haakon.foodImage + ")");
+  $(".overlay-title").html(food[2].haakon.shopName);
+  $(".overlay-rating").append(food[2].haakon.foodRating);
+  $(".overlay-ambience").append(food[2].haakon.ambienceRating);
+
+  var pin = document.createElement("img");
+  pin.src = "img/icons/pin.png";
+  var src = document.getElementById("food-location");
+  src.appendChild(pin);
+  $(".overlay-location").append(food[2].haakon.shopLocation);
+
+  $(".overlay-description").html(food[2].haakon.foodDescription);
+
+  $("#thumbnail-1").attr("src", food[2].haakon.thumbnail1);
+  $("#thumbnail-2").attr("src", food[2].haakon.thumbnail2);
+  $("#thumbnail-3").attr("src", food[2].haakon.thumbnail3);
+ }
